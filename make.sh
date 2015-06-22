1e393c69c7c6a88db22f9b4ee637b3f6e1cfc8d3
Mark Seaborn <mseaborn@chromium.org>
2015-06-22 07:01:19 -0700

    Only try building double_sided_rowhammer.cc on Linux
    This program requires Linux and doesn't build on Mac OS X.

--- make.sh
+++ make.sh
@@ -19,4 +19,7 @@ set -eu
 cflags="-g -Wall -Werror -O2"
 
 g++ $cflags rowhammer_test.cc -o rowhammer_test
-g++ $cflags -std=c++11 double_sided_rowhammer.cc -o double_sided_rowhammer
+
+if [ "$(uname)" = Linux ]; then
+  g++ $cflags -std=c++11 double_sided_rowhammer.cc -o double_sided_rowhammer
+fi
